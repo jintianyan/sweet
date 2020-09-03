893f5dc028509e0dd9ed317d9528aaf37c2b3f1b
jintianyan <1436335453@qq,com>
2020-09-03 11:28:30 +0800

    1、增加eureka子模块作为注册服务端 2、将单maven项目改造成maven父子项目

--- eureka/src/main/java/com/sweet/EurekaApplication.java
+++ eureka/src/main/java/com/sweet/EurekaApplication.java
@@ -1,13 +1,13 @@
-package com.example.sweet;
+package com.sweet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SweetApplication {
+public class EurekaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SweetApplication.class, args);
+		SpringApplication.run(EurekaApplication.class, args);
 	}
 
 }
